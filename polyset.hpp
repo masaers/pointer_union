9e846a0e4ef8788587136b282ecd7e195d2db162
masaers <masaers@gmail.com>
2014-12-17 11:23:50 +0800

    Improved polyset

--- polyset.hpp
+++ polyset.hpp
@@ -1,6 +1,7 @@
 #ifndef COM_MASAERS_POLYSET_HPP
 #define COM_MASAERS_POLYSET_HPP
 
+#include "pointer_union.hpp"
 #include <utility>
 #include <set>
 
@@ -11,12 +12,18 @@ namespace com_masaers {
 
 	template<>
 	class polyset<> {
-	public:
-		template<typename T> void insert(T&& value) {
-			static_assert(std::is_same<T, void>::value, "Cannot insert unsupported type into a polyset.");
+	protected:
+		template<typename T> const void* do_find(const T& value) const {
+		  static_assert(std::is_same<T, void>::value, "Cannot find unsupported type in a polyset.");
+		  return NULL;
 		}
-		template<typename T> void find(T&& value) const {
-  		static_assert(std::is_same<T, void>::value, "Cannot find unsupported type in a polyset.");
+		template<typename T> std::pair<const void*, bool> do_insert(const T& value) {
+			static_assert(std::is_same<T, void>::value, "Cannot insert unsupported type into a polyset.");
+		  return std::pair<const void*, bool>(NULL, false);
+		}
+	public:
+	  template<typename T> void erase(T&& value) {
+	    static_assert(std::is_same<T, void>::value, "Cannot erase unsupported type in a polyset.");
 	  }
 	  bool empty() const { return true; }
 	  std::size_t size() const { return 0; }
@@ -28,26 +35,43 @@ namespace com_masaers {
 	protected:
 		typedef polyset<Values...> base_type;
 		typedef std::set<Value> store_type;
+		typedef ptrunion<typename std::add_const<Value>::type, typename std::add_const<Values>::type...> ptr_type;
 		store_type store_m;
-	public:
-		auto insert(const Value& value) -> decltype(this->store_m.insert(value)) {
+		const void* do_find(const Value& value) const {
+			const void* result = NULL;
+			auto it = store_m.find(value);
+			if (it != store_m.end()) {
+				result = static_cast<const void*>(&*it);
+			}
+			return result;
+		}
+		template<typename T> const void* do_find(const T& value) const {
+		  return base_type::do_find(value);
+		}
+		std::pair<typename store_type::iterator, bool> do_insert(const Value& value) {
 			return store_m.insert(value);
 		}
-		template<typename T> auto insert(const T& value) -> decltype(base_type::insert(value)) {
-			return base_type::insert(value);
+		template<typename T> auto do_insert(const T& value) -> decltype(base_type::do_insert(value)) {
+			return base_type::do_insert(value);
 		}
-		auto find(const Value& value) -> decltype(this->store_m.find(value)) {
-			return store_m.find(value);
+	public:
+		template<typename T> ptr_type find(const T& value) const {
+		  return ptr_type(static_cast<const typename std::decay<T>::type*>(do_find(value)));
 		}
-		template<typename T> auto find(const T& value) -> decltype(base_type::find(value)) {
-		  return base_type::find(value);
+		template<typename T> std::pair<ptr_type, bool> insert(const T& value) {
+		  auto p = do_insert(value);
+		  return std::pair<ptr_type, bool>(static_cast<const typename std::decay<T>::type*>(&*p.first), p.second);
+		}
+		decltype(std::declval<store_type>().erase(std::declval<const Value&>()))
+		erase(const Value& value) {
+			return store_m.erase(value);
+		}
+		template<typename T> auto erase(const T& value) -> decltype(base_type::erase(value)) {
+			return base_type::erase(value);
 		}
 		bool empty() const { return store_m.empty() && base_type::empty(); }
 		std::size_t size() const { return store_m.size() + base_type::size(); }
-		void clear() {
-			store_m.clear();
-			base_type::clear();
-		}
+		void clear() { store_m.clear(); base_type::clear(); }
 	}; // polyset inductive case
 
 } // namespace com_masaers
--- polyset_test.cpp
+++ polyset_test.cpp
@@ -10,19 +10,26 @@ int main(const int argc, const char** argv) {
   polyset<double, int> ps;
   {
     auto p = ps.insert(double(1.5));
-    cout << *p.first << " " << (p.second ? "true" : "false") << endl;
+    cout << *(const double*)p.first << " " << (p.second ? "true" : "false") << endl;
   }
   {
     auto p = ps.insert(int(1));
-    cout << *p.first << " " << (p.second ? "true" : "false") << endl;
+    cout << *(const int*)p.first << " " << (p.second ? "true" : "false") << endl;
   }
   {
     auto p = ps.insert(int(1));
-    cout << *p.first << " " << (p.second ? "true" : "false") << endl;
+    cout << *(const int*)p.first << " " << (p.second ? "true" : "false") << endl;
   }
   {
     auto it = ps.find(int(1));
-    cout << *it << endl;  
+    cout << *(const int*)it << endl;  
+  }
+  {
+    auto it = ps.find(int(1));
+    cout << ((const int*)it != NULL ? "found!" : "missing!") << endl;
+    ps.erase(int(1));
+    auto jt = ps.find(int(1));
+    cout << ((const int*)jt != NULL ? "found!" : "missing!") << endl;
   }
   return EXIT_SUCCESS;
 }
